1a48ec6d96eebd04c8b86eddaf79a23a28de2b08
Uwe Roder <uweroder@gmail.com>
2019-11-10 12:15:38 +0100

    fixed tests

--- loggingDaemon/tests/Utils/DataStorageTest/DataStorageTest.cpp
+++ loggingDaemon/tests/Utils/DataStorageTest/DataStorageTest.cpp
@@ -6,15 +6,6 @@ using namespace std;
 
 TEST(DataStorageTest, getCommandOption_negative_test) {
 
-    DataStorage dataStorage;
+    DataStorage<string> dataStorage;
     set<string> messages = dataStorage.readMessages();
-
-    // LoggingProcessor& loggingProcessor = LoggingProcessor::getInstance();
-    // loggingProcessor.enableTerminal();
-
-    // const char* argv[] = { "ArgumentParserTest", "--logfile", "/var/log/daemon", "param2" };
-    // const int argc = 3;
-    // ArgumentParser argumentParser(argc, argv);
-
-    // EXPECT_EQ("", argumentParser.getCommandOption("--blubb"));
 }
